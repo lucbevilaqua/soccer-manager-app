36c52fe12b75a0e9a74a515d7ba53ffa69c5e325
Lucas B. Araujo de Oliveira <laobevilaqua@outlook.com>
2021-01-30 11:28:30 -0300

    nova service utils

--- src/pages/DrawTeam/index.js
+++ src/pages/DrawTeam/index.js
@@ -3,6 +3,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import Team from '~/components/Team';
 import getRealm from '~/services/realm';
+import shuffle, { randomInteger, separator } from '~/services/utils';
 
 import {
   Container,
@@ -16,15 +17,6 @@ export default function DrawTeam() {
   const REPOSITORY = 'User';
   const [users, setUsers] = useState([]);
   const [teams, setTeams] = useState([]);
-
-  Array.prototype.shuffle = function() {
-    let m = this.length, i;
-    while (m) {
-      i = (Math.random() * m--) >>> 0;
-      [this[m], this[i]] = [this[i], this[m]]
-    }
-    return this;
-  }
 
   useEffect(() => {
     async function loadRepository() {
@@ -45,26 +37,10 @@ export default function DrawTeam() {
     loadRepository();
   }, []);
 
-  function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 
   function draw() {
     setUsers(users.shuffle());
     createTeams();
-  }
-
-  function separator(base, max) {
-    const res = [];
-    const parts = (base.length + 1)/max;
-    const baseCopy = base.map((item) => item);
-
-    console.log(baseCopy)
-    for (let part = 0; part < parts; part++) {
-      res.push(baseCopy.splice(0, max));
-    }
-
-    return res;
   }
 
   function createTeams() {
---
+++ src/services/utils.js
@@ -0,0 +1,27 @@
+
+
+
+export default Array.prototype.shuffle = function() {
+  let m = this.length, i;
+  while (m) {
+    i = (Math.random() * m--) >>> 0;
+    [this[m], this[i]] = [this[i], this[m]]
+  }
+  return this;
+}
+
+export function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function separator(base, max) {
+  const res = [];
+  const parts = (base.length + 1)/max;
+  const baseCopy = base.map((item) => item);
+
+  for (let part = 0; part < parts; part++) {
+    res.push(baseCopy.splice(0, max));
+  }
+
+  return res;
+}
